706cd963e519894fd0715c131c3a3fbe9c66d0a8
adajoy <305039615@qq.com>
2017-04-26 21:10:56 +0800

    add lesson tools in welcome

---
+++ front-mensheng-vue/src/pages/welcome/tool.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="tools w1140 clearFix">
+    <div class="head">
+      <div class="icon"></div>
+      <p>课堂工具</p>
+    </div>
+    <ul class="clearFix">
+      <li><img src="./images/tool1.png" alt="" width="170" height="147"></li>
+      <li><img src="./images/tool2.png" alt="" width="170" height="147"></li>
+      <li><img src="./images/tool3.png" alt="" width="170" height="147"></li>
+      <li><img src="./images/tool4.png" alt="" width="170" height="147"></li>
+    </ul>
+  </div>
+</template>
+<script>
+	export default {
+  name: 'tool'
+	}
+</script>
+<style scoped>
+	.tools{height:250px;border-bottom: 2px solid #ccc;position: relative;}
+  .tools .head{float:left;line-height: 26px;padding: 10px;background: rgb(189,204,212);}
+  .tools .head .icon{width: 26px;height: 26px;float: left; background: url("images/qietu.png") -199px -123px;}
+  .tools .head p{display: inline-block;text-indent: 5px;padding-right: 60px;color: #fff;}
+  .tools ul{position: absolute;width:100%;left: 0;bottom: 0;margin-bottom: 20px;}
+  .tools ul li{display:block;float: left;width: 285px;}
+  .tools ul li img{display:block;margin: auto;}
+</style> 
--- front-mensheng-vue/src/router/index.js
+++ front-mensheng-vue/src/router/index.js
@@ -2,18 +2,24 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import login from '@/pages/welcome/login'
 import register from '@/pages/welcome/register'
+import tool from '@/pages/welcome/tool'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/dl',
+      path: '/tool',
+      name: 'tool',
+      component: tool
+    },
+    {
+      path: '/login',
       name: 'login',
       component: login
     },
     {
-      path: '/zc',
+      path: '/register',
       name: 'register',
       component: register
     }
