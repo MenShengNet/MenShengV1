a3ba8cd62aa6d3cf9e33f9a87aa4831729568fc3
zzhutianyu <704735206@qq.com>
2017-05-19 12:55:05 +0800

    update course

--- front-mensheng-vue/src/pages/course/course_work_detail.vue
+++ front-mensheng-vue/src/pages/course/course_work_detail.vue
@@ -1,28 +1,28 @@
 <template>
 	<div class="article">
-    <div class="rightbox box"><h2>考试</h2></div>
-    <hr >
-    <h2>期中考试</h2>
-    <div class="kuai51">
-    <p class="head1">考试内容：</p>
-    <div><p class="txtcontent">这里考试内容啦啦啦啦啦啦啦啦啦啦啦这里考试内容啦啦啦啦啦啦啦啦啦啦啦这里考试内容啦啦啦啦啦啦啦啦啦啦啦这里考试内容啦啦啦啦啦啦啦啦啦啦啦这里考试内容啦啦啦啦啦啦啦啦啦啦啦这里考试内容啦啦啦啦啦啦啦啦啦啦啦这里考试内容啦啦啦啦啦啦啦啦啦啦啦这里考试内容啦啦啦啦啦啦啦啦啦啦啦这里考试内容啦啦啦啦啦啦啦啦啦啦啦这里考试内容啦啦啦啦啦啦啦啦啦啦啦这里考试内容啦啦啦啦啦啦啦啦啦啦啦这里考试内容啦啦啦啦啦啦啦啦啦啦啦这里考试内容啦啦啦啦啦啦啦啦啦啦啦</p></div>
+	<div class="rightbox box"><h2>作业</h2></div>
+	<hr >
+    <h2>第一周&nbsp;&nbsp;&nbsp;&nbsp;作业XXXX</h2>
+	<div class="kuai31">
+    <p class="head1">作业内容：</p>
+    <div class="txt"><p class="txtcontent">这里作业内容啦啦啦啦啦啦啦啦啦啦啦这里作业内容啦啦啦啦啦啦啦啦啦啦啦这里作业内容啦啦啦啦啦啦啦啦啦啦啦这里作业内容啦啦啦啦啦啦啦啦啦啦啦这里作业内容啦啦啦啦啦啦啦啦啦啦啦这里作业内容啦啦啦啦啦啦啦啦啦啦啦这里作业内容啦啦啦啦啦啦啦啦啦啦啦这里作业内容啦啦啦啦啦啦啦啦啦啦啦这里作业内容啦啦啦啦啦啦啦啦啦啦啦这里作业内容啦啦啦啦啦啦啦啦啦啦啦这里作业内容啦啦啦啦啦啦啦啦啦啦啦这里作业内容啦啦啦啦啦啦啦啦啦啦啦这里作业内容啦啦啦啦啦啦啦啦这里作业内容啦</p></div>
     <textarea cols="132" rows="25"></textarea>
     <button> 上传附件 </button>
     <p class="instruction">支持txt、mp3、jpg、png、rar、zip、doc、ppt、xls、pdf等格式，文件最大50M。只支持上传一个附件，重复上传将会被覆盖。</p> 
     <input type="submit" value="提   交" text-align="right">
     </div>
-
 </div>
 </template>
-<script type="text/javascript">
+<script>
 	export default {
-		name: 'exam_detail'
+		name: 'work_detail'
 	}
 </script>
 <style scoped>
-	    .article ,.article div{
-        display: block;
-    }
+
+	.article, .article div{
+		display: block;
+	}
     .article hr{
         width:5;
         color: #c2cacf;
@@ -41,10 +41,10 @@
         font-size: 20px; 
     }
     .article {
-        width: 960px;
+    	width: 960px;
         margin: auto;
     }
-    .rightbox {
+	.rightbox {
         width: 720px;
         margin-right: 10px;
      }
@@ -69,17 +69,17 @@
         width: 208px;
         border: white 1px solid;
     }  
-    .rightbox h2 {
-        color: #c2cacf;
+	.rightbox h2 {
+		color: #c2cacf;
         font-family: "宋体",sans-serif;
         padding: 5px;
     }
-    .kuai51 {
-        border: 2px #c2cacf solid;
-        margin-top: 15px;
-        margin-right: 15px;
-        width: 960px;
-        padding: 0px;
-        display: inline-block;
-    }
+	.kuai31 {
+		border: 2px #c2cacf solid;
+    	margin-top: 15px;
+    	margin-right:15px;
+    	width: 960px;
+    	padding: 0px;
+    	display: inline-block;
+	}
 </style>
